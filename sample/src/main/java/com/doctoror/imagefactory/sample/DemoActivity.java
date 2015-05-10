b6ada7cb8c2c78cc048f30a9f64597584e3a5912
koral <koral>
2015-05-10 03:18:23 +0200

    MultiDrawable added

--- sample/src/main/java/com/doctoror/imagefactory/sample/DemoActivity.java
+++ sample/src/main/java/com/doctoror/imagefactory/sample/DemoActivity.java
@@ -20,11 +20,13 @@ import com.doctoror.imagefactory.ImageFactory;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
@@ -32,8 +34,12 @@ import android.widget.ImageView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class DemoActivity extends ActionBarActivity {
 
@@ -117,8 +123,57 @@ public final class DemoActivity extends ActionBarActivity {
                 imageView = (ImageView) convertView.getTag();
             }
 
-            imageView.setImageDrawable(getItem(position));
+            final Drawable item = getItem(position);
+            Drawable.Callback callback = item.getCallback();
+            imageView.setImageDrawable(item);
+            if (!(callback instanceof MultiCallback)) {
+                callback = new MultiCallback();
+            }
+            ((MultiCallback) callback).addView(imageView);
+            item.setCallback(callback);
+
             return convertView;
+        }
+    }
+
+    public static class MultiCallback implements Drawable.Callback {
+        private Set<WeakReference<View>> mViewSet = new LinkedHashSet<>();
+
+        @Override
+        public void invalidateDrawable(Drawable who) {
+            Iterator<WeakReference<View>> iterator = mViewSet.iterator();
+            while (iterator.hasNext()) {
+                final View view = iterator.next().get();
+                if (view != null) {
+                    view.invalidateDrawable(who);
+                }
+            }
+        }
+
+        @Override
+        public void scheduleDrawable(Drawable who, Runnable what, long when) {
+            Iterator<WeakReference<View>> iterator = mViewSet.iterator();
+            while (iterator.hasNext()) {
+                final View view = iterator.next().get();
+                if (view != null) {
+                    view.scheduleDrawable(who, what, when);
+                }
+            }
+        }
+
+        @Override
+        public void unscheduleDrawable(Drawable who, Runnable what) {
+            Iterator<WeakReference<View>> iterator = mViewSet.iterator();
+            while (iterator.hasNext()) {
+                final View view = iterator.next().get();
+                if (view != null) {
+                    view.unscheduleDrawable(who, what);
+                }
+            }
+        }
+
+        public void addView(View view) {
+            mViewSet.add(new WeakReference<>(view));
         }
     }
 }
